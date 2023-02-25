b6798d8740da8fa0f97abc48b6958389490fb5bd
Suraksha <surakshak571@gmail.com>
2023-02-25 23:52:19 +0500

    game ready

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 var paper = 'paper.png';
 var rock ='rock.png';
 var scissor='scissor.png';
-var marks;
+var marks = 0;
 var choice;
+var score =  document.getElementById('settxt');
  function func(){
     choice = 1;
     document.getElementById('human').src = paper;
     computer();
-    
+    document.getElementById('mark').innerHTML = marks;
  }
  
  function func1(){choice = 2;
     document.getElementById('human').src = rock;
     computer();
-    
+    document.getElementById('mark').innerHTML = marks;
  }
  
  function func2(){
       choice = 3 ;
     document.getElementById('human').src = scissor;
     computer();
+    document.getElementById('mark').innerHTML = marks;
  }
 
 // get random number
@@ -28,20 +30,22 @@ var rnd =  Math.random();
 console.log(rnd);
 if(rnd <= 0.333){
     document.getElementById('comp').src = paper;
-    if(choice == 1){document.getElementById('result').innerHTML="Tie";}
-    else{document.getElementById('result').innerHTML="Won"; marks=marks+1;}
+   if(choice == 1){ marks = marks + 0; score.innerText="Tie";}
+   else if(choice == 2){ score.innerText="You Lost";}
+   else{marks = marks + 1; score.innerText="You Win";}
 }
 else if(rnd <= 0.67 ){
     document.getElementById('comp').src = rock;
-    if(choice == 1){document.getElementById('result').innerHTML="Won"; marks=marks+1;}
-    else if(choice == 2){document.getElementById('result').innerHTML="Tie";}
-    else{document.getElementById('result').innerHTML="Lost"; marks=0; }
+    if(choice == 1){ marks = marks + 1; score.innerText="You Win";}
+    else if(choice == 2){marks = marks + 0; score.innerText="Tie";}
+    else{ score.innerText="You lost";}
 }
 else{
     document.getElementById('comp').src = scissor;
-    marks=marks+1;
-    if(choice == 1){document.getElementById('result').innerHTML=marks; }
-    else if(choice == 2){document.getElementById('result').innerHTML="Lost"; marks=0;}
-    else{document.getElementById('result').innerHTML="Tie";}
+    if(choice == 1){  score.innerText="You Lost";}
+   else if(choice == 2){marks = marks + 1; score.innerText="You Win";}
+   else{marks = marks + 1; score.innerText="Tie";}
 }
+
 }
+
